e9ebbcf8b21d38d4a2bbe5ce335656a706b0cc35
Samuel[D[D[D[D[D[D(Samuel Tesfou[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[[[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D <samueltesfou7@gmail.com>
2024-04-14 16:15:45 -0400

    sending backend changes

--- vue/src/services/SpoonService.js
+++ vue/src/services/SpoonService.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const baseUrl = "https://api.spoonacular.com/recipes/";
-<<<<<<< HEAD
-const apiKey= "&apiKey=cd2c9521530b4c31bd45126b77f2cdcf";
-=======
 const apiKey= "&apiKey=250b663cb5c4495fb447c0c6ab6f005e";
->>>>>>> 910e19a8d936356b2ed431ec9b6c7c42de11a1a0
 
 
 const newAxiosInstance = axios.create();
